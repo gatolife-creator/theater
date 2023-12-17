92f627b18924fc669d215c514769be516934c3a8
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-12-17 11:12:29 +0900

    fix: small css bug

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <>
-      <div className="navbar fixed left-2 top-3 w-[calc(100%-1rem)] rounded-3xl bg-blue-400/30 shadow-lg backdrop-blur-sm">
+      <div className="navbar fixed left-2 top-3 z-[9999] w-[calc(100%-1rem)] rounded-3xl bg-blue-400/30 shadow-lg backdrop-blur-sm">
         <Link to="/" className="btn btn-ghost text-xl">
           Theater
         </Link>
