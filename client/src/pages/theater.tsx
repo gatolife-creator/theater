ea5595d1bc0b7e456dc638e184efe02fe08f6034
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-12-19 17:37:30 +0900

    add: streaming support

--- client/src/pages/theater.tsx
+++ client/src/pages/theater.tsx
@@ -1,36 +1,18 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Player } from "video-react";
 import { Main } from "../components/Main";
 
 export const Theater = () => {
   const [searchParams] = useSearchParams();
-  const [video, setVideo] = useState("");
   const id = searchParams.get("watch");
 
-  useEffect(() => {
-    fetch("/api/video", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw "動画取得エラー";
-        }
-        return res.blob();
-      })
-      .then((blob) => {
-        setVideo(URL.createObjectURL(blob).toString());
-      });
-  }, []);
+  const videoUrl = `/api/video?id=${id}`;
 
   return (
     <Main>
       <div style={{ width: 600, margin: "0 auto" }}>
-        <Player src={video} autoPlay />
+        <Player src={videoUrl} autoPlay />
       </div>
     </Main>
   );
--- server/api/videos.ts
+++ server/api/videos.ts
@@ -18,7 +18,7 @@ const database = new Database(new Map(Object.entries(JSON.parse(json))));
 router.get("/table", async (req: express.Request, res: express.Response) => {
   await getTable(req, res);
 });
-router.post("/video", async (req: express.Request, res: express.Response) => {
+router.get("/video", async (req: express.Request, res: express.Response) => {
   await getVideo(req, res);
 });
 router.post(
@@ -51,8 +51,35 @@ async function getTable(_: express.Request, res: express.Response) {
 
 async function getVideo(req: express.Request, res: express.Response) {
   try {
-    const id = req.body.id;
-    res.sendFile(path.join(videosDirectory, `${id}.mp4`));
+    const id = req.query.id;
+    console.log(`Sending video ${id}`);
+    const videoPath = path.join(videosDirectory, `${id}.mp4`);
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunksize = end - start + 1;
+      const file = fs.createReadStream(videoPath, { start, end });
+      const head = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        "Content-Length": fileSize,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(videoPath).pipe(res);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
